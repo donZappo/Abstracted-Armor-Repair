767aee17a6ba6f80b36406981a0afb47c9a8c6dd
Bradley Burcar <btburcar@gmail.com>
2020-03-16 03:02:53 -0400

    Add project files.

---
+++ Abstracted Amor Repair/Core.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using Harmony;
+using Newtonsoft.Json;
+using static AbstractedArmorRepair.Logger;
+using BattleTech;
+using BattleTech.UI;
+
+namespace AbstractedArmorRepair
+{
+    public static class Core
+    {
+        #region Init
+
+        public static void Init(string modDir, string settings)
+        {
+            var harmony = HarmonyInstance.Create("AbstractedArmorRepari.donZappo");
+            // read settings
+            try
+            {
+                Settings = JsonConvert.DeserializeObject<ModSettings>(settings);
+                Settings.modDirectory = modDir;
+            }
+            catch (Exception)
+            {
+                Settings = new ModSettings();
+            }
+
+            // blank the logfile
+            Clear();
+            PrintObjectFields(Settings, "Settings");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+        // logs out all the settings and their values at runtime
+        internal static void PrintObjectFields(object obj, string name)
+        {
+            LogDebug($"[START {name}]");
+
+            var settingsFields = typeof(ModSettings)
+                .GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var field in settingsFields)
+            {
+                if (field.GetValue(obj) is IEnumerable &&
+                    !(field.GetValue(obj) is string))
+                {
+                    LogDebug(field.Name);
+                    foreach (var item in (IEnumerable)field.GetValue(obj))
+                    {
+                        LogDebug("\t" + item);
+                    }
+                }
+                else
+                {
+                    LogDebug($"{field.Name,-30}: {field.GetValue(obj)}");
+                }
+            }
+
+            LogDebug($"[END {name}]");
+        }
+
+        #endregion
+
+        internal static ModSettings Settings;
+    }
+}
---
+++ Abstracted Amor Repair/ModSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BattleTech;
+
+
+namespace AbstractedArmorRepair
+{
+    public class ModSettings
+    {
+        public bool Debug = false;
+        public string modDirectory;
+
+        public bool RepairRearm = true;
+    }
+}
